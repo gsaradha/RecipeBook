e923ccebb40f5d613040603d9cde43561a73d046
gsaradha <saradha.girish@gmail.com>
2021-03-11 19:19:20 -0800

    Basic recipe design

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe','This is simple Recipe','../../../assets/Images/choc.jpg'),
+    new Recipe('A Test Recipe','This is simple Recipe','../../../assets/Images/soup.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
